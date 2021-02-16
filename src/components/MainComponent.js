2357358f7e9ccc3bd9c5719f98ad1b8dfb17551a
AlexisHoneycutt <honeycuttam@gmail.com>
2021-02-16 18:03:29 -0500

    Redux Actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { addComment } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
 	return {
@@ -16,6 +17,11 @@ const mapStateToProps = (state) => {
 		partners: state.partners,
 		promotions: state.promotions,
 	};
+};
+
+const mapDispatchToProps = {
+	addComment: (campsiteId, rating, author, text) =>
+		addComment(campsiteId, rating, author, text),
 };
 
 class Main extends Component {
@@ -55,6 +61,7 @@ class Main extends Component {
 						(comment) =>
 							comment.campsiteId === +match.params.campsiteId
 					)}
+					addComment={this.props.addComment}
 				/>
 			);
 		};
@@ -89,4 +96,4 @@ class Main extends Component {
 	}
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
